6c6b1f232476d95e42b39e5da4619dc1460d9a67
Ed Lingo <ejlingo@gmail.com>
2018-08-21 18:52:28 -0700

    Add project files.

---
+++ CS_155_Lingo_Edward/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CS_155_Lingo_Edward
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
